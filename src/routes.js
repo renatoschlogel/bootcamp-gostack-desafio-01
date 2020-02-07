335750d0dad97c745cd131be51735adb2bf535cb
Renato Welinton Schlogel <rschlogel@hotmail.com>
2020-02-07 01:05:40 -0300

    ajustes

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,37 @@ import { Router } from "express";
 
 const routes = new Router();
 
+let sequence_id = 1;
+const projects = [
+  {
+  id:1,
+  title: "Primeiro Projeto",
+  tasks: ["Primeira Tarefa"]
+}]
+
+function getNextId(){
+  return ++sequence_id;
+}
+
 routes.post('/projects', (req, res) => {
-  res.json({ msg: "Agora vai" });
+  res.json(projects);
 });
 
 routes.get('/projects', (req, res) => {
-  res.json({ msg: "Agora vai" });
+  res.json(projects);
+});
+
+routes.get('/projects/:id', (req, res) => {
+  let id = req.params.id
+  let project = projects.find(function(project){
+    return project.id == id;
+  });
+
+  if (project) {
+    return res.json(project);
+  }
+  
+  return res.json({erro: "Projeto não encontrado com id ".concat(id)});
 });
 
 routes.put('/projects/:id', (req, res) => {
